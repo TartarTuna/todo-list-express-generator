4fbd8df6761c66a2c453d68be767c4678336247b
Wei-Yun <flyling503@gmail.com>
2022-04-27 23:58:03 +0800

    feat(post.js): arrange router

--- models/postsModel.js
+++ models/postsModel.js
@@ -2,18 +2,40 @@ const mongoose = require('mongoose')
 
 const postSchema = new mongoose.Schema(
   {
-    title: {
+    tags: [
+      {
+        type: String,
+        required: [true, '標籤不能為空字串']
+      },
+    ],
+    type: {
       type: String,
-      required: [true, "title 未填寫"],
+      enum: ['group', 'person']
+    },
+    name: {
+      type: String,
+      required: [true, "名稱為必填"]
+    },
+    content: {
+      type: String,
+      required: [true, '內容為必填']
     },
     image: {
       type: String,
-      default: "",
+      default: ''
     },
     createdAt: {
       type: Date,
-      default: Date.now(),
-      select: false,
+      default: Date.now,
+      select: false
+    },
+    likes: {
+      type: Number,
+      default: 0
+    },
+    comments: {
+      type: Number,
+      default: 0
     }
   },
   {
--- routes/posts.js
+++ routes/posts.js
@@ -11,52 +11,99 @@ router.get('/', async function (req, res, next) {
 })
 
 router.post('/', async function (req, res, next) {
-  const data = req.body
-  const newPost = await Post.create(data)
+  try {
+    const data = req.body
+    if (data.content) {
+      const newPost = await Post.create(data)
+      res.status(200).json({
+        status: 'success',
+        newPost
+      })
+    } else {
+      res.status(400).json({
+        status: 'false',
+        message: '格式錯誤或缺少必填欄位'
+      })
+    }
 
-  res.status(200).json({
-    status: 'success',
-    newPost
-  })
+  } catch (err) {
+    res.status(400).json({
+      status: 'false',
+      message: err.message
+    })
+  }
 })
 
 router.delete('/', async function (req, res, next) {
-  await Post.deleteMany({})
-
-  res.status(200).json({
-    status: 'success'
-  })
+  try {
+    await Post.deleteMany({})
+    res.status(200).json({
+      status: 'success'
+    })
+  } catch (err) {
+    res.status(400).json({
+      status: 'false',
+      message: err.message
+    })
+  }
 })
 
 router.delete('/:id', async function (req, res, next) {
-  const id = req.params.id
-  await Post.findByIdAndDelete(id)
-  const posts = await Post.find()
-
-  res.status(200).json({
-    status: 'success',
-    posts
-  })
+  try {
+    const id = req.params.id
+    const delPost = await Post.findByIdAndDelete(id)
+    if (delPost) {
+      const posts = await Post.find()
+      res.status(200).json({
+        status: 'success',
+        posts
+      })
+    } else {
+      res.status(400).json({
+        status: 'false',
+        message: '格式錯誤或 id 錯誤'
+      })
+    }
+  } catch (err) {
+    res.status(400).json({
+      status: 'false',
+      message: err.message
+    })
+  }
 })
 
 router.patch('/:id', async function (req, res, next) {
-  const id = req.params.id
-  const title = req.body.title
-  const image = req.body.image
+  try {
+    const id = req.params.id
+    const data = req.body
 
-  const newPost = await Post.findByIdAndUpdate(
-    id,
-    {
-      title,
-      image
-    },
-    { new: true }
-  )
+    if (data.content) {
+      const newPost = await Post.findByIdAndUpdate(id, data)
+      if (newPost) {
+        const post = await Post.find()
+        res.status(200).json({
+          status: 'success',
+          post
+        })
+      } else {
+        res.status(400).json({
+          status: 'false',
+          message: 'id 填寫錯誤'
+        })
+      }
+    } else {
+      res.status(400).json({
+        status: 'false',
+        message: '格式錯誤或必填欄位未填'
+      })
+    }
 
-  res.status(200).json({
-    status: 'success',
-    newPost
-  })
+  } catch (err) {
+    res.status(400).json({
+      status: 'false',
+      message: err.message
+    })
+  }
 })
 
 module.exports = router
